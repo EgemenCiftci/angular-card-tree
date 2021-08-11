5ff19c55d1982ea62831be5ef09e227ce74de310
EgemenCiftci <egemenciftci@gmail.com>
2021-08-11 19:30:24 +0300

    4 files modified

--- src/app/card.ts
+++ src/app/card.ts
@@ -3,6 +3,8 @@ import { Container } from './container';
 export class Card {
   label: string;
   containers: Container[];
+  width: number;
+  height: number;
 
   addContainer(container: Container) {
     if (!this.containers) {
--- src/app/container.ts
+++ src/app/container.ts
@@ -3,6 +3,8 @@ import { Card } from './card';
 export class Container {
   label: string;
   cards: Card[];
+  height: number;
+  width: number;
 
   addCard(card: Card) {
     if (!this.cards) {
